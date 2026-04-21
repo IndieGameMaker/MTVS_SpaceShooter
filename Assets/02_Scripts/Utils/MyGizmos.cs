21222352b5e64abc6b4ba25998405c5f2e400d27
Lee JaeHyun <myleje@gmail.com>
2026-04-21 10:16:11 +0900

    feat(GameManager): GameManager 생성

--- Assets/02_Scripts/Utils/MyGizmos.cs
+++ Assets/02_Scripts/Utils/MyGizmos.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class MyGizmos : MonoBehaviour
 {
+    public enum DrawType
+    {
+        Sphere,
+        Icon
+    }
+
+    [SerializeField] private DrawType _drawType = DrawType.Sphere;
     [SerializeField] private Color _color = Color.green;
-    [Range(1f, 5f)] 
-    [SerializeField] private float _radius = 1f;
+    [Range(1f, 5f)] [SerializeField] private float _radius = 1f;
 
     private void OnDrawGizmos()
     {
         // 기즈모 색상 설정
         Gizmos.color = _color;
-        // Sphere 생성
-        Gizmos.DrawSphere(transform.position, _radius);
+        if (_drawType == DrawType.Sphere)
+        {
+            // Sphere 생성
+            Gizmos.DrawSphere(transform.position, _radius);
+        }
+        else
+        {
+            // Icon 출력
+            Gizmos.DrawIcon(transform.position, "Enemy.png");
+        }
     }
 }
