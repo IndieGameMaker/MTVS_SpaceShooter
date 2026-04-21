939e8cf60159c161caaaae38422fe08f5296bfcb
Lee JaeHyun <myleje@gmail.com>
2026-04-21 13:40:21 +0900

    feat:트레일 렌더러 초기화

--- Assets/02_Scripts/Weapon/Bullet.cs
+++ Assets/02_Scripts/Weapon/Bullet.cs
@@ -6,9 +6,12 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _force = 1200.0f;
 
+    private TrailRenderer _trail;
+    
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _trail = GetComponent<TrailRenderer>();
     }
 
     public void Fire(Vector3 pos, Quaternion rot)
@@ -18,6 +21,9 @@ public class Bullet : MonoBehaviour
         // 물리 속성 초기화
         _rb.linearVelocity = _rb.angularVelocity = Vector3.zero;
         _rb.rotation = rot;
+        
+        // 트레일 렌더러 초기화
+        _trail.Clear();
         
         // 뉴튼(N)
         _rb.AddRelativeForce(Vector3.forward * _force);
