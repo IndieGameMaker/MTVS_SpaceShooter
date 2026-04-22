c4e000ee9bb36c25f22483bb0a81443c5cc59802
Lee JaeHyun <myleje@gmail.com>
2026-04-22 13:54:28 +0900

    feat: UIManaer

--- Assets/02_Scripts/Manager/UIManager.cs
+++ Assets/02_Scripts/Manager/UIManager.cs
@@ -1,10 +1,30 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private Button _startButton;
+
+    private void OnEnable()
+    {
+        // 버튼 클릭 이벤트 구독(Subscribe)
+        _startButton.onClick.AddListener(OnStartButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        // 이벤트 구독 해지
+        _startButton.onClick.RemoveListener(OnStartButtonClick);
+    }
+
     public void OnStartButtonClick()
     {
         // 씬 로딩
-        Debug.Log("시작버튼 클릭됨");
+        // Debug.Log("시작버튼 클릭됨");
+        SceneManager.LoadScene("InGame");  // 씬의 이름을 로딩
+        // SceneManager.LoadScene(1);  // 씬의 Index으로 로딩
+        
     }
 }
